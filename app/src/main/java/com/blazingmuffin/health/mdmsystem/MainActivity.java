6aa4bfd5cfe90556cc72465341d97b3892e288e8
Miko Chu <miko@alliedworld.healthcare>
2017-10-21 09:18:50 +0000

    Merged in feature-navigation (pull request #7)
    Feature navigation
    
    * Fragment navigation using Navigational Drawer
    
    * Change Toolbar layout parent to DarkActionBar
    
    * Change ActionBar to NoAction bar in themes.
    
    Approved-by: Paolo Balleza <paolo.balleza@gmail.com>

--- app/src/main/java/com/blazingmuffin/health/mdmsystem/MainActivity.java
+++ app/src/main/java/com/blazingmuffin/health/mdmsystem/MainActivity.java
@@ -1,18 +1,43 @@
 package com.blazingmuffin.health.mdmsystem;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+implements NavigationView.OnNavigationItemSelectedListener {
 
     private FragmentManager mManager;
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private Toolbar mToolbar;
+    private NavigationView mNavigationDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_general_layout);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close);
+        mActionBarDrawerToggle.syncState();
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mNavigationDrawer = (NavigationView) findViewById(R.id.navigation_view);
+        mNavigationDrawer.setNavigationItemSelectedListener(this);
 
         mManager = getSupportFragmentManager();
 //
@@ -27,5 +52,31 @@ public class MainActivity extends AppCompatActivity {
         transaction.add(R.id.linear_fragment_container, settingsFragment, "resident");
         transaction.commit();
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        FragmentTransaction fragmentTransaction = mManager.beginTransaction();
+        switch (item.getItemId()) {
+            case R.id.item_residents:
+                ResidentFragment residentFragment = new ResidentFragment();
+                fragmentTransaction.replace(R.id.linear_fragment_container, residentFragment, "resident");
+                break;
+            case R.id.item_settings:
+                SettingsFragment settingsFragment = new SettingsFragment();
+                fragmentTransaction.replace(R.id.linear_fragment_container, settingsFragment, "resident");
+                break;
+        }
+        fragmentTransaction.commit();
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mActionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
