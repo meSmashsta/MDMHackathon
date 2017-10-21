c5fec09cb0e6863f3eab508edaf4dd2f7b0ac81d
Miko Chu <miko@alliedworld.healthcare>
2017-10-21 11:11:24 +0800

    Fragment feature

--- app/src/main/java/com/blazingmuffin/health/mdmsystem/MainActivity.java
+++ app/src/main/java/com/blazingmuffin/health/mdmsystem/MainActivity.java
@@ -1,13 +1,31 @@
 package com.blazingmuffin.health.mdmsystem;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager mManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mManager = getSupportFragmentManager();
+//
+//        ResidentFragment residentFragment = new ResidentFragment();
+//        FragmentTransaction transaction = mManager.beginTransaction();
+//        transaction.add(R.id.linear_fragment_container, residentFragment, "resident");
+//        transaction.commit();
+
+
+        SettingsFragment settingsFragment = new SettingsFragment();
+        FragmentTransaction transaction = mManager.beginTransaction();
+        transaction.add(R.id.linear_fragment_container, settingsFragment, "resident");
+        transaction.commit();
+
     }
 }
---
+++ app/src/main/java/com/blazingmuffin/health/mdmsystem/SettingsFragment.java
@@ -0,0 +1,21 @@
+package com.blazingmuffin.health.mdmsystem;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lenovo on 10/21/2017.
+ */
+
+public class SettingsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings_layout, container, false);
+        return view;
+    }
+}
