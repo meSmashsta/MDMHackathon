c55b33d22ff20837f0b3dd18c8b6f840978bee12
Paolo Balleza <paolo.balleza@gmail.com>
2017-10-21 21:07:24 +0000

    Merged in cleanup-main-activity (pull request #25)
    Cleanup
    
    Approved-by: Kristhian Tiu <b.kristhian.tiu@gmail.com>

--- app/src/main/java/com/blazingmuffin/health/mdmsystem/MainActivity.java
+++ app/src/main/java/com/blazingmuffin/health/mdmsystem/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -17,24 +18,17 @@ import android.text.format.Formatter;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.blazingmuffin.health.mdmsystem.other.models.MDMContext;
 import com.blazingmuffin.health.mdmsystem.other.models.ResidentEntity;
 import com.blazingmuffin.health.mdmsystem.other.repositories.ResidentRepository;
-import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Database;
 import com.couchbase.lite.Manager;
 import com.couchbase.lite.listener.LiteListener;
 import com.couchbase.lite.replicator.Replication;
 
-import java.io.IOException;
-import java.net.InetAddress;
 import java.net.MalformedURLException;
-import java.net.NetworkInterface;
-import java.net.SocketException;
 import java.net.URL;
-import java.util.Enumeration;
 import java.util.HashMap;
 
 import de.mannodermaus.rxbonjour.BonjourBroadcastConfig;
@@ -46,11 +40,10 @@ import io.reactivex.CompletableObserver;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private static String TAG = "JEEPERS : MainActivity";
+    private static final String TAG = "JEEPERS : MainActivity";
     private ResidentRepository mResidentRepository;
     private ResidentEntity mResidentEntity;
 
@@ -58,30 +51,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mActionBarDrawerToggle;
-    private Toolbar mToolbar;
-    private NavigationView mNavigationDrawer;
     private FloatingActionButton mFloatingAddButton;
 
 
     private String mSyncURLString = "";
-    private String mRESTURLString;
 
     // couchbase
     private static Manager mCBManager;
     private Database mDatabase;
     private LiteListener mRESTListener;
-    public static final String designDocName = "resident-view";
-    public static final String byDateViewName = "byDate";
     private Boolean mIsCBSyncRunning = false;
     private Replication mPullReplication;
     private Replication mPushReplication;
 
     // service discovery and registration
-    protected RxBonjour mRxBonjour;
-
-    public RxBonjour getRxBonjour() {
-        return mRxBonjour;
-    }
+    private RxBonjour mRxBonjour;
 
     public void setSyncURLString(String syncURLString) {
         mSyncURLString = syncURLString;
@@ -93,7 +77,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 
 
-        mToolbar = findViewById(R.id.toolbar_general_layout);
+        Toolbar mToolbar = findViewById(R.id.toolbar_general_layout);
         setSupportActionBar(mToolbar);
 
         Database database = MDMContext.Instance(MainActivity.this);
@@ -103,8 +87,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close);
         mActionBarDrawerToggle.syncState();
         mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mNavigationDrawer = findViewById(R.id.navigation_views);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        NavigationView mNavigationDrawer = findViewById(R.id.navigation_views);
         mNavigationDrawer.setNavigationItemSelectedListener(this);
         mFloatingAddButton = findViewById(R.id.fab);
 
@@ -168,50 +155,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     private void startServices(){
-        try {
-            startCouchbaseLite();
-            startCBSyncing();
-            startRESTListener();
-            //mRESTURLString = getRESTListenerURL();
-            //mExperimentUrlText.setText(mRESTURLString);
-            mRxBonjour = (new RxBonjour.Builder()).platform(AndroidPlatform.create(this)).driver(JmDNSDriver.create()).create();
-            broadcastService();
-            // call discover on Peer Fragment
-            //startDiscover();
-        } catch (CouchbaseLiteException e){
-            Toast.makeText(getApplicationContext(), "DB : Get Database went wrong.", Toast.LENGTH_LONG);
-            Log.d(TAG, "Can not create initialize CB Lite");
-        }  catch (IOException e){
-            Toast.makeText(getApplicationContext(), "DB : Initialization went wrong.", Toast.LENGTH_LONG);
-            Log.d(TAG, "Can not create initialize CB Lite");
-        }
+        startCouchbaseLite();
+        startCBSyncing();
+        startRESTListener();
+        //mRESTURLString = getRESTListenerURL();
+        //mExperimentUrlText.setText(mRESTURLString);
+        mRxBonjour = (new RxBonjour.Builder()).platform(AndroidPlatform.create(this)).driver(JmDNSDriver.create()).create();
+        broadcastService();
+        // call discover on Peer Fragment
+        //startDiscover();
         Log.d(TAG, "DONE!");
     }
 
 
-    private void startCouchbaseLite() throws IOException, CouchbaseLiteException {
+    private void startCouchbaseLite() {
         mCBManager = MDMContext.Manager(this);
         mDatabase = MDMContext.Instance(this);
-
-        /*
-        Manager.enableLogging(TAG, Log.VERBOSE);
-        mCBManager = new Manager(new AndroidContext(getApplicationContext()), Manager.DEFAULT_OPTIONS);
-        DatabaseOptions dbOptions = new DatabaseOptions();
-        dbOptions.setCreate(true);
-        String db = getString(R.string.general_database_name);
-        mDatabase = mCBManager.openDatabase(db, dbOptions);
-        // TODO: 10/21/17 PUT Create View here
-        com.couchbase.lite.View viewItemsByDate = mDatabase.getView(String.format("%s/%s", designDocName, byDateViewName));
-        viewItemsByDate.setMap(new Mapper() {
-            @Override
-            public void map(Map<String, Object> document, Emitter emitter) {
-                Object createdAt = document.get("created_at");
-                if (createdAt != null) {
-                    emitter.emit(createdAt.toString(), null);
-                }
-            }
-        }, "1.0");
-        */
     }
 
     protected void startCBSyncing(){
@@ -239,12 +198,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mPullReplication = mDatabase.createPullReplication(syncURL);
         mPullReplication.setContinuous(true);
         mPullReplication.start();
-        mPullReplication.addChangeListener(new Replication.ChangeListener() {
-            @Override
-            public void changed(Replication.ChangeEvent event) {
-                // TODO: 10/21/17 PUT PULL Replication Event handler here
-                Log.d(TAG, "PULL : change detected!");
-            }
+        mPullReplication.addChangeListener(event -> {
+            // TODO: 10/21/17 PUT PULL Replication Event handler here
+            Log.d(TAG, "PULL : change detected!");
         });
     }
 
@@ -252,12 +208,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mPushReplication = mDatabase.createPushReplication(syncURL);
         mPushReplication.setContinuous(true);
         mPushReplication.start();
-        mPushReplication.addChangeListener(new Replication.ChangeListener() {
-            @Override
-            public void changed(Replication.ChangeEvent event) {
-                // TODO: 10/21/17 PUT PUSH Replication Event handler here
-                Log.d(TAG, "PUSH : change detected!");
-            }
+        mPushReplication.addChangeListener(event -> {
+            // TODO: 10/21/17 PUT PUSH Replication Event handler here
+            Log.d(TAG, "PUSH : change detected!");
         });
     }
 
@@ -267,43 +220,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         thread.start();
     }
 
-    private String getRESTListenerURL() throws MalformedURLException {
-        String restUrlString = "";
-        String ipadd = getIPAddressDeprecated();
-        int port = mRESTListener.getListenPort();
-        URL url = new URL("http", ipadd, port, "/" + getString(R.string.general_database_name));
-        return url.toString();
-    }
-
-    private String getIPAddress(){
-        String IPAddress = "";
-        try {
-            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
-                NetworkInterface intf = en.nextElement();
-                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
-                    InetAddress inetAddress = enumIpAddr.nextElement();
-                    if (!inetAddress.isLoopbackAddress() && !inetAddress.isLinkLocalAddress() && inetAddress.isSiteLocalAddress())  {
-                        IPAddress = inetAddress.getHostAddress().toString();
-                        break;
-                    }
-
-                }
-            }
-        } catch (SocketException ex) {
-            Log.e("LOG_TAG", ex.toString());
-            IPAddress = "";
-        }
-        return IPAddress;
-    }
-
     private String getIPAddressDeprecated(){
         @SuppressLint("WifiManagerLeak") WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
-        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
-        return ip;
+        return Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
     }
 
     private void broadcastService(){
-        HashMap<String, String> mss1 = new HashMap<String, String>();
+        HashMap<String, String> mss1 = new HashMap<>();
         mss1.put("my.record","my value");
         mss1.put("other.record","0815");
         String serviceName = getString(R.string.general_database_name);
@@ -331,13 +254,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected Observable<BonjourEvent> startDiscover (){
         return mRxBonjour.newDiscovery("_http._tcp")
                 .filter(
-                        new Predicate<BonjourEvent>() {
-                            @Override
-                            public boolean test(@io.reactivex.annotations.NonNull BonjourEvent bonjourEvent) throws Exception {
-                                Boolean is_diff_address = bonjourEvent.getService().getHost().getHostAddress().compareTo(getIPAddressDeprecated()) != 0;
-                                Boolean is_correct_service = bonjourEvent.getService().getName().startsWith(getString(R.string.general_database_name));
-                                return is_correct_service && is_diff_address;
-                            }
+                        bonjourEvent -> {
+                            Boolean is_diff_address = bonjourEvent.getService().getHost().getHostAddress().compareTo(getIPAddressDeprecated()) != 0;
+                            Boolean is_correct_service = bonjourEvent.getService().getName().startsWith(getString(R.string.general_database_name));
+                            return is_correct_service && is_diff_address;
                         }
                 );
                 //.filter(e -> e.getService().getHost().getHostAddress().compareTo(getIPAddressDeprecated()) == -1)
@@ -345,7 +265,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 //.subscribeOn(Schedulers.io());
     }//--startDiscover
 
-    protected void stopRunningSync(){
+    private void stopRunningSync(){
         if(mIsCBSyncRunning){
             mPullReplication.stop();
             mPushReplication.stop();
