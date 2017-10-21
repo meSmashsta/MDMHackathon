b0f87e93f0241b21b35420aa320741867d415597
Philip  Sales <pj_sal87@yahoo.com>
2017-10-21 14:25:25 +0000

    Merged in feature-materialUI (pull request #13)
    Feature materialUI
    
    * Add RecyclerView dependency
    
    * Modify gradle for correct RecyclerView version dependency.
    
    * Add materialUI branch
    
    * Fragment navigation using Navigational Drawer
    
    * Add colorthemes
    
    * Add theme colors and pictures.
    
    * Add app icon
    
    * Remove string duplicate
    
    * Remove conflict duplicate functions
    
    Approved-by: Paolo Balleza <paolo.balleza@gmail.com>

---
+++ app/src/main/java/com/blazingmuffin/health/mdmsystem/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.blazingmuffin.health.mdmsystem;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+}
