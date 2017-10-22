ced1745026421264266c11014e602282989de41c
Kristhian Tiu <b.kristhian.tiu@gmail.com>
2017-10-22 00:15:42 +0000

    Merged in feature-upload-server (pull request #28)
    Feature upload server
    
    * Show IP Address only on list of peers
    
    * Color of connect button - similar to floating Add Button
    
    * Floating Fab bar hides on other fragments
    
    * Floating button position at the end of the page always
    
    * Renamed drawer menus and change icons
    
    * Settings : UI Changed to Upload
    
    * Idea
    
    * Hide Unused ui elements on settings
    
    Approved-by: Paolo Balleza <paolo.balleza@gmail.com>

--- app/src/main/java/com/blazingmuffin/health/mdmsystem/SettingsFragment.java
+++ app/src/main/java/com/blazingmuffin/health/mdmsystem/SettingsFragment.java
@@ -6,16 +6,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by lenovo on 10/21/2017.
  */
 
 public class SettingsFragment extends Fragment {
+
+    private Button mUploadButton;
+    private EditText mServerUrlText;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings_layout, container, false);
+
+        String serverUrlString = getString(R.string.general_default_sgw);
+        mServerUrlText = view.findViewById(R.id.server_url_text);
+        mServerUrlText.setText(serverUrlString);
+        mUploadButton = view.findViewById(R.id.upload_button);
+        mUploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).setSyncURLString(serverUrlString);
+                ((MainActivity) getActivity()).startCBSyncing();
+            }
+        });
+
         return view;
     }
 }
